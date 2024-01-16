2b8a63faab9098e44df661b4b61f6ff6da876ce9
Andres Munoz <ganonx9@gmail.com>
2024-01-16 18:13:03 -0500

    finished mesh rendering and test 1 and 2

--- comp557a1.py
+++ comp557a1.py
@@ -19,88 +19,138 @@ pix = np.zeros((width, height, 3), dtype=np.float32)
 depth = np.zeros((width, height, 1), dtype=np.float32)
 pixti = ti.Vector.field(3, dtype=ti.f32, shape=(width, height))
 pixels = ti.Vector.field(3, dtype=ti.f32, shape=(width*px, height*px))
-V, _, N, T, _, TN = igl.read_obj(args.file) #read mesh with normals
+Vertices, _, Normals, Tris, _, TN = igl.read_obj(args.file) #read mesh with normals
 
+def drawToPix(x,y,r=1,g=1,b=1):
+    pix[x,y,0]=r
+    pix[x,y,1]=g
+    pix[x,y,2]=b
 
-
-
-# exit()
-
-def project(Point3d):
-    x,y,z=Point3d
-    x+=.5
-    y+=.5
-    v=y/z
+def projectAndScale(Point3D,scale=1):    
+    x,y,z=Point3D
+    z=1 #note this makes it orthographic, but can change if necessary
     u=x/z
-    return [u,v]
-
-def toWorldCoordinates(objectCoordinates2D,scale=1):
-    oX,oY=objectCoordinates2D
+    v=y/z    
     originX= width/2
     originY=height/2
-    worldX=originX+oX*scale
-    worldY=originY+oY*scale
-    return np.clip([worldX,worldY],[0,0],[width-1,height-1])
+    worldX=originX+u*scale
+    worldY=originY+v*scale
+    #clip to fit in the buffer
+    return np.clip([worldX,worldY],[0,0],[width-1,height-1]).astype(int)
+
 
 def drawTri(triangle):
-    for vIndex in triangle:
-        x,y=V2dWorldCoords[vIndex]
-        pix[x,y,0]=0
-        pix[x,y,1]=255
-        pix[x,y,2]=0
+    vertex3d=np.zeros((3,3))
+    for i,vIndex in enumerate(triangle):
+        vertex3d[i]=Vertices[vIndex]
 
-    
-    p1=V2dWorldCoords[triangle[0]]
-    p2=V2dWorldCoords[triangle[1]]
-    p3=V2dWorldCoords[triangle[2]]
+    scale =500
+    p1=projectAndScale(vertex3d[0],scale)    
+    p2=projectAndScale(vertex3d[1],scale)    
+    p3=projectAndScale(vertex3d[2],scale)
 
+    #these values have already been clipped to be within the buffer 
     boundingBoxMinX= min(p1[0],p2[0],p3[0])
     boundingBoxMinY= min(p1[1],p2[1],p3[1])
     boundingBoxMaxX= max(p1[0],p2[0],p3[0])
     boundingBoxMaxY= max(p1[1],p2[1],p3[1])
 
+    
+
     if args.test == 1:
         r=random.rand()
         g=random.rand()
         b=random.rand()
-        for i in range(boundingBoxMinX,boundingBoxMaxX):
-            for j in range(boundingBoxMinY,boundingBoxMaxY):
-                pix[i,j,0]=r
-                pix[i,j,1]=g
-                pix[i,j,2]=b
-    else:
-        for i in range(boundingBoxMinX,boundingBoxMaxX):
-            for j in range(boundingBoxMinY,boundingBoxMaxY):
-                alpha,beta,gamma=cartesianToBary([i,j],[p2,p3,p1])
+        for x in range(boundingBoxMinX,boundingBoxMaxX):
+            for y in range(boundingBoxMinY,boundingBoxMaxY):
+                drawToPix(x,y,r,g,b)
+    elif args.test==2:
+        for x in range(boundingBoxMinX,boundingBoxMaxX):
+            for y in range(boundingBoxMinY,boundingBoxMaxY):
+                alpha,beta,gamma=cartesianToBary2D([x,y],[p2,p3,p1])
                 if alpha>0 and beta>0 and gamma>0:
-                    pix[i,j,0]=alpha
-                    pix[i,j,1]=beta
-                    pix[i,j,2]=gamma
-    # drawLine(p1,p2)
-    # drawLine(p2,p3)
-    # drawLine(p3,p1)
+                    z = alpha*vertex3d[0][2]+beta*vertex3d[1][2]+gamma*vertex3d[2][2]
+                    if(depth[x,y] < z):
+                        depth[x,y]=z
+                        drawToPix(x,y,alpha,beta,gamma)
+    else:
+        #todo interpolate normal vectors
+        drawLine(p1,p2)
+        drawLine(p2,p3)
+        drawLine(p3,p1)
 
-def cartesianToBary(point,triangleVertices):
+
+#todo this was taken form wikipedia, try to understand it later
+def drawLine(p1,p2):
+    x0,y0=p1
+    x1,y1=p2
+    if abs(y1-y0)<abs(x1-x0):
+        if x0>x1:
+            plotLineLow(x1,y1,x0,y0)
+        else:
+            plotLineLow(x0,y0,x1,y1)
+    else:
+        if y0>y1:
+            plotLineHigh(x1,y1,x0,y0)
+        else:
+            plotLineHigh(x0,y0,x1,y1)
+
+def plotLineLow(x0,y0,x1,y1):
+    dx=x1-x0
+    dy=y1-y0
+    yi=1
+    if dy<0:
+        yi=-1
+        dy=-dy
+    D=(2*dy)-dx
+    y=y0
+    for x in range(x0,x1):
+        drawToPix(x,y)
+        if D>0:
+            y=y+yi
+            D=D+(2*(dy-dx))
+        else:
+            D=D+2*dy
+
+def plotLineHigh(x0,y0,x1,y1):
+    dx=x1-x0
+    dy=y1-y0
+    xi=1
+    if dx<0:
+        xi=-1
+        dx=-dx
+    D=(2*dx)-dy
+    x=x0
+    for y in range(y0,y1):
+        drawToPix(x,y)
+        if D>0:
+            x=x+xi
+            D=D+(2*(dx-dy))
+        else:
+            D=D+2*dx
+def cartesianToBary2D(point2D,triangleVertices):
     a, b, c = triangleVertices    
-    x1,y1=a
-    x2,y2=b
-    x3,y3=c
-    x, y = point    
+    xa,ya=a
+    xb,yb=b
+    xc,yc=c
+    x, y = point2D        
     # Calculate the barycentric coordinates
-    det = (y2-y3)*(x1-x3)+(x3-x2)*(y1-y3)    
-    if det==0:
-        return 1,1,1
-    alpha=((y2-y3)*(x-x3)+(x3-x2)*(y-y3))/det
-    beta=((y3-y1)*(x-x3)+(x1-x3)*(y-y3))/det
+    triangleArea = (yb-yc)*(xa-xc)+(xc-xb)*(ya-yc)    
+    if triangleArea==0:
+        return 0,0,0
+    alpha=((yb-yc)*(x-xc)+(xc-xb)*(y-yc))/triangleArea
+    beta=((y-yc)*(xa-xc)+(xc-x)*(ya-yc))/triangleArea
     gamma=1-alpha-beta    
     return alpha, beta, gamma
 
+def triangleNormal(triangleVertices3D):
+    a,b,c =triangleVertices3D
+    a=np.array(a)
+    b=np.array(b)
+    c=np.array(c)
+    n = np.cross(np.subtract(b,a),np.subtract(c,a))
+    return n
 
-V2d= np.array([project(p) for p in V])
-V2dWorldCoords=np.array([toWorldCoordinates(p,scale=10) for p in V2d], dtype=int)
-
-
-# exit()
 
 @ti.kernel
 # copy pixels from small framebuffer to large framebuffer
@@ -115,26 +165,20 @@ translate = np.array([ width/2,height/2,0 ]) # translate to center of window
 scale = 200/px*np.eye(3) # scale to fit in the window
 while gui.running:
     pix.fill(0) # clear pixel buffer
-    # for x,y in V2dWorldCoords:
-    #     pix[x,y,0]=255
-    #     pix[x,y,1]=255
-    #     pix[x,y,2]=255
-    for triangle in T:
-        drawTri(triangle)
-
-
-
     depth.fill(-math.inf) # clear depth buffer
     #time varying transformation
-    c,s = math.cos(1.2*t),math.sin(1.2*t)
-    Ry = np.array([[ c, 0, s],[ 0, 1, 0],[-s, 0, c]])
-    c,s = math.cos(t),math.sin(t)
-    Rx = np.array([[ 1, 0, 0],[ 0, c, s],[ 0,-s, c]])
-    c,s = math.cos(1.8*t),math.sin(1.8*t)
-    Rz = np.array([[ c, s, 0],[-s, c, 0],[ 0, 0, 1]])
-    Vt = (scale @ Ry @ Rx @ Rz @ V.T).T
+    cos,sin = math.cos(1.2*t),math.sin(1.2*t)
+    Rotation_y = np.array([[ cos, 0, sin],[ 0, 1, 0],[-sin, 0, cos]])
+    cos,sin = math.cos(t),math.sin(t)
+    Rotation_x = np.array([[ 1, 0, 0],[ 0, cos, sin],[ 0,-sin, cos]])
+    cos,sin = math.cos(1.8*t),math.sin(1.8*t)
+    Rotation_z = np.array([[ cos, sin, 0],[-sin, cos, 0],[ 0, 0, 1]])
+    Vt = (scale @ Rotation_y @ Rotation_x @ Rotation_z @ Vertices.T).T
     Vt = Vt + translate
-    Nt = (Ry @ Rx @ Rz @ N.T).T
+    Nt = (Rotation_y @ Rotation_x @ Rotation_z @ Normals.T).T
+
+    for triangle in Tris:
+        drawTri(triangle)
     # draw!
     pixti.from_numpy(pix)
     copy_pixels()
